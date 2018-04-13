1404fb4951138bfaeaf419bf10706cf54373867f
haven-jeon <>
2018-04-14 00:52:44 +0900

    remove direct access from pkg

--- R/onLoad.R
+++ R/onLoad.R
@@ -10,6 +10,7 @@
 
 
 #' @importFrom reticulate import import_builtins py_module_available use_condaenv conda_create conda_install conda_list
+#' @importFrom keras load_model_hdf5
 #' @import hashmap
 .onAttach <- function(libname, pkgname){
   envnm <-'r-conda'
@@ -44,7 +45,7 @@
   assign("c2idx", w2idx_tbl, envir=.KoSpacingEnv)
 
   model_file <- file.path(system.file(package="KoSpacing"),"model", 'kospacing')
-  model <- keras::load_model_hdf5(model_file)
+  model <- load_model_hdf5(model_file)
   packageStartupMessage("loaded KoSpacing model!")
   assign("model", model, envir=.KoSpacingEnv)
 }
