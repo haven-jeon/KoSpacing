42b0016b50bf3eac0bd72102979c653beed51a9e
Chanyub Park <mrchypark@gmail.com>
2018-09-18 03:56:44 +0900

    change .onAttech to set_env
    change .onAttech to set_env

---
+++ R/set_env.R
@@ -0,0 +1,45 @@
+#' set conda env using kospacing function.
+#'
+#' @importFrom reticulate import import_builtins py_module_available use_condaenv conda_create conda_install conda_list
+#' @importFrom keras load_model_hdf5
+#' @import hashmap
+#' @export
+set_env <- function() {
+  envnm <- 'r-conda'
+
+  tryCatch({
+    if (!(envnm %in% conda_list()$name)) {
+      conda_create(envnm)
+    }
+  },
+  error = function(e) {
+    stop("Need to install Anaconda3(>=3.6) from https://www.anaconda.com/download/.")
+  },
+  finally = {
+    use_condaenv(envnm, required = TRUE)
+    if (!py_module_available("tensorflow")) {
+      conda_install(envnm, packages = c('tensorflow=1.4.0'))
+    }
+
+    if (!py_module_available("keras")) {
+      conda_install(envnm, packages = c('keras=2.1.5'))
+    }
+
+    if (!py_module_available("h5py")) {
+      conda_install(envnm, packages = c('h5py'))
+    }
+  })
+
+  w2idx <-
+    file.path(system.file(package = "KoSpacing"), "model", 'w2idx')
+
+  w2idx_tbl <- load_hashmap(w2idx)
+
+  assign("c2idx", w2idx_tbl, envir = .KoSpacingEnv)
+
+  model_file <-
+    file.path(system.file(package = "KoSpacing"), "model", 'kospacing')
+  model <- load_model_hdf5(model_file)
+  packageStartupMessage("loaded KoSpacing model!")
+  assign("model", model, envir = .KoSpacingEnv)
+}
